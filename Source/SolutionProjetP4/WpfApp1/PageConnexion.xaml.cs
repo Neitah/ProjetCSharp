942c9a2f414ef8008aafaf4ad6e3c1c35a7bd342
mamaulus1 <mamaulus1@133772c0-da04-490a-aa5e-9dbb43a63b65>
2021-04-09 08:51:10 +0000

    création page connexion

---
+++ Source/SolutionProjetP4/WpfApp1/PageConnexion.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logique d'interaction pour PageConnexion.xaml
+    /// </summary>
+    public partial class PageConnexion : Window
+    {
+        public PageConnexion()
+        {
+            InitializeComponent();
+        }
+    }
+}
