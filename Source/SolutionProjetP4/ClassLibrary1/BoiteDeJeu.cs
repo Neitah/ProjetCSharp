36babde5354899917056ab1baebf1cfbcdb29be1
mamaulus1 <mamaulus1@133772c0-da04-490a-aa5e-9dbb43a63b65>
2021-04-29 10:05:47 +0000

    nouveau commit car l'ancien --> erreur

---
+++ Source/SolutionProjetP4/ClassLibrary1/BoiteDeJeu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    class BoiteDeJeu : Nommable
+    {
+        public string Description { get; set; }
+        public string NomMagasin { get; set; }
+        public BoiteDeJeu(string description,string nomMagasin, string nom) : base(nom)
+        {
+            Description = description;
+            NomMagasin = nomMagasin;
+        }
+    }
+}
---
+++ Source/SolutionProjetP4/TestProfils/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace TestProfils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test de la création d'un profil\n");
+
+            Console.WriteLine("-----------------------------------------------");
+
+            Console.WriteLine("\nCréation de 5 compétences : \n");
+
+            Compétence c1 = new Compétence("Description compétences 1", "Nom compétences 1");
+            Console.WriteLine(c1);
+            Compétence c2 = new Compétence("Description compétences 2", "Nom compétences 2");
+            Console.WriteLine(c2);
+            Compétence c3 = new Compétence("Description compétences 3", "Nom compétences 3");
+            Console.WriteLine(c3);
+            Compétence c4 = new Compétence("Description compétences 4", "Nom compétences 4");
+            Console.WriteLine(c4);
+            Compétence c5 = new Compétence("Description compétences 5", "Nom compétences 5");
+            Console.WriteLine(c5);
+            Compétence c6 = new Compétence("Description compétences 6", "Nom compétences 6");
+            Console.WriteLine(c6);
+
+            Console.WriteLine("\n-----------------------------------------------");
+
+            Console.WriteLine("\nCréation d'une voie : \n");
+
+            Voie v1 = new Voie("Voie 1");
+            Console.WriteLine(v1);
+            v1.AjoutCompétence(c1);
+            v1.AjoutCompétence(c2);
+            v1.AjoutCompétence(c3);
+            v1.AjoutCompétence(c4);
+            v1.AjoutCompétence(c5);
+            Console.WriteLine(v1);
+
+            Console.WriteLine("\n-----------------------------------------------");
+
+            Console.WriteLine("\nCréation d'un profil : \n");
+            Profil p1 = new Profil("Nom Profil 1", "dé de Vie", "équipement", "armes et armures", "divers");
+            Console.WriteLine(p1);
+            p1.AjoutVoie(v1);
+            p1.AjoutVoie(v1);
+            Console.WriteLine(p1);
+        }
+    }
+}
