97331e9ca8b68c42e5b9fc76e5af9ffacac5a09e
mamaulus1 <mamaulus1@133772c0-da04-490a-aa5e-9dbb43a63b65>
2021-04-30 09:53:18 +0000

    ajout de commentaire sur les classes Utilisateurs,... + ToString dans classe Utilisateur + Méthode SupprimerProfilHybride + test sur utilisateurs

--- Source/SolutionProjetP4/ClassLibrary1/Nommable.cs
+++ Source/SolutionProjetP4/ClassLibrary1/Nommable.cs
@@ -5,7 +5,16 @@ namespace ClassLibrary1
 {
     public abstract class Nommable
     {
+
+        /// <summary>
+        /// Nom d'un Objet 
+        /// </summary>
         public string Nom { get; set; }
+
+        /// <summary>
+        /// Constructeur 
+        /// </summary>
+        /// <param name="nom"> Nom d'un Objet </param>
         public Nommable(string nom)
         {
             Nom = nom;
---
+++ Source/SolutionProjetP4/TestUtilisateur/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace TestUtilisateur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test de la création d'un profil\n");
+
+            Console.WriteLine("-----------------------------------------------\n");
+
+            Utilisateur u1 = new Utilisateur("Nom utilisateur 1", "Mot de passe utilisateur 1");
+            Console.WriteLine(u1);
+
+        }
+    }
+}
